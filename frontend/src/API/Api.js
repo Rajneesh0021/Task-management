7e7aa72a2a0eda2f50b7373be3ab08a898aa80a1
rajneesh <mr.rajneeshy@gmail.com>
2024-05-20 09:14:02 +0530

    changed entry point

--- frontend/src/API/Api.js
+++ frontend/src/API/Api.js
@@ -1,66 +1,9 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:9090/api/V0';
-
-// Fetch all tasks
-export const fetchTasks = async (token) => {
-  try {
-    const response = await axios.get(baseURL, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    });
-    return response.data;
-    
-  } catch (error) {
-    return error
-  }
-};
-// create a task 
-export const createTask = async (payload,token) => {
-  try {
-    const response = await axios.post(baseURL,payload,  {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    });
-    return response.data;
-    
-  } catch (error) {
-    return error
-  }
-};
-// Update a task
-export const updateTask = async (taskId, updatedTask, token) => {
-  try {
-    const response = await axios.put(`${baseURL}/${taskId}`, updatedTask, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    });
-    return response.data;
-  } catch (error) {
-   return error
-  }
-};
-
-// Delete a task
-export const deleteTask = async (taskId, token) => {
-  try {
-   
-    const response = await axios.delete(`${baseURL}/${taskId}`, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    });
-    return response.data;
-  } catch (error) {
-    return error
-  }
-};
+const baseURL = 'https://task-management-o8ye.onrender.com/api/V0';
 
 
-// Register a new user or login
+//
 export const APICall = async (payload,token, endpoint,method) => {
   try {
     const response = await axios({
